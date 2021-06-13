c939ac1aadaee21a6a6830810c4ffa3fe70a0f1b
reedperkins <reedperkins32@gmail.com>
2021-06-13 16:23:12 -0600

    more CSS styling changes

--- testdb.py
+++ testdb.py
@@ -1,14 +1,17 @@
 import requests
 import json
 import random
+import time
 
-
+# URL = 'http://127.0.0.1:5000/api/assignment'
+URL = 'http://mind.cs.byu.edu:3000/api/assignment'
 for i in range(10):
     for i in range(25):
+        time.sleep(random.randint(1, 5))
         email = str(i)
         option = random.choice(['first', 'returning'])
         r = requests.request('POST', 
-            'http://127.0.0.1:5000/api/assignment', 
+            URL, 
             data=json.dumps({
                 'email': email,
                 'option': option
